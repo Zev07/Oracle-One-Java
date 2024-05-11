8979d7761732c284a00fe2e74f04d94b23f3a8c8
Zev <90863634+Zev07@users.noreply.github.com>
2024-05-11 08:36:18 -0300

    Add files via upload

---
+++ Práticas/src/Ex3.java
@@ -0,0 +1,10 @@
+public class Ex3 {
+    public static void main(String[] args) {
+        char letra = 'A';
+        String palavra = "ZUL";
+
+        String mensagem = "A letra é " + letra + " e a palavra é " + letra + palavra;
+
+        System.out.println(mensagem);
+    }
+}

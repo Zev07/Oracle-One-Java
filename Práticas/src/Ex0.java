1ce226ab647484a384f8fb5f747d942e2fbc6593
Zev <90863634+Zev07@users.noreply.github.com>
2024-05-11 08:25:50 -0300

    Add files via upload

---
+++ Práticas/src/Ex0.java
@@ -0,0 +1,9 @@
+public class Ex0{
+    public static void main(String[] args) {
+        double nota1 = 6.0;
+        double nota2 = 6.6;
+        double nota3 = 6.4;
+        double media = (nota1 + nota2 + nota3) / 3 ;
+        System.out.println("A média foi " + media);
+    }
+}
---
+++ Práticas/src/Ex1.java
@@ -0,0 +1,7 @@
+public class Ex1 {
+    public static void main(String[] args) {
+        double numeroDouble = 15.7;
+        int numeroInteiro = (int) numeroDouble;
+        System.out.println("Casting de double e inteiro: " + numeroInteiro);
+    }
+}
